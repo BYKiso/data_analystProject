27df80b4a0f3d670d4f80832c09e1cb92b55aeea
BYKiso <bk495@student.aru.ac.uk>
2024-11-10 19:36:29 +0000

    initial  commit

---
+++ maths.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+data = [2, 3, 5, 4]
+index = ['a', 'b', 'c', 'd']
+
+# Create a structured array to mimic a Series
+dtype = [('index', 'U1'), ('data', 'i4')]
+structured_array = np.array(list(zip(index, data)), dtype=dtype)
+
+
+def print_structured_array(arr):
+    print("index  data")
+    for item in arr:
+        print(f"{item['index']}     {item['data']}")
+
+
+print_structured_array(structured_array)
